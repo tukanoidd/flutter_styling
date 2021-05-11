a6210021b6b53867c26f2e1989390a897086f031
Vitalii Lukyanov <tukanoid@outlook.com>
2021-05-11 19:38:51 +0200

    Update list.dart

--- lib/extensions/list.dart
+++ lib/extensions/list.dart
@@ -3,8 +3,8 @@ part of '../tukanoid_styling.dart';
 /// [List] extension helper functions
 extension ListExtension on List {
   /// Return a sorted list with optional [compare] function to do the sorting
-  List sorted([int Function(dynamic, dynamic)? compare]) {
-    List newList = List.from(this);
+  List<T> sorted<T>([int Function(T, T)? compare]) {
+    List<T> newList = List.from(this);
     newList.sort(compare);
 
     return newList;
